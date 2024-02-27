1d4cf1fe8c8014150f7be02c74cb9f2917bf09e6
Qweyke <kettenkrad1337@yandex.ru>
2024-02-27 23:36:55 +0500

    Добавьте файлы проекта.

---
+++ Lab1CS/AbstractList.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace Lab1CS
+{
+    class ArrList
+    {
+        int cnt = 0;
+        int[] buf = null;
+        int size = 1;
+        public void Init()
+        {
+            buf = new int[size];
+        }
+
+        private void Expd()
+        {
+            size *= 2;
+            Array.Resize(ref buf, size);
+        }
+
+        public void Add(int val)
+        {
+            if (cnt >= size) { Expd(); }
+            buf[cnt] = val;
+            cnt++;
+
+        }
+        public void Ins(int val, int pos)
+        {
+
+            if (pos < 0 || pos > cnt)
+            {
+                throw new ArgumentOutOfRangeException($"Invalid position {pos} for insertion");
+            }
+
+            if (pos <= cnt && pos >= 0)
+            {
+                if (cnt >= size) { Expd(); }
+                for (int i = pos; i == cnt; i++)
+                {
+                    buf[i + 1] = buf[i];
+                }
+                buf[pos] = val;
+                cnt++;
+            }
+        }
+        public void Del(int pos)
+        {
+            if (pos <= cnt)
+            {
+                for (int i = pos; i <= cnt; i++)
+                {
+                    buf[i] = buf[i+1];
+                }
+                cnt--;
+            }
+        }
+
+        public void Clr()
+        {
+            for (int i = 0; i < cnt; i--)
+            {
+                buf[i] = 0;
+            }
+            cnt = 0;
+        }
+
+        public int Count
+        {
+            get { return cnt; }
+        }
+
+        public int this[int i]
+        { 
+            get {
+                if (cnt <= i || i < 0)
+                {
+                    throw new IndexOutOfRangeException($"There is no index {i} in array"); 
+                }
+
+                return buf[i]; }
+
+            set {
+                if (cnt <= i || i < 0)
+                {
+                    throw new IndexOutOfRangeException($"There is no index {i} in array");
+                }
+                
+                buf[i] = value; }
+        }
+        public void Shw()
+        {
+            if (cnt >= 0)
+            {
+                for (int i = 0; i <= cnt; i++)
+                {
+                    Console.Write($"{buf[i]} ");
+                }
+            }
+        }
+    }
+}
---
+++ Lab1CS/LinkedList.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+
+
+namespace Lab1CS
+{
+    public class ChainList
+    {
+        public class Node //private?
+        {
+            public int Data
+            {
+                set; get;
+            }
+            public Node Next
+            {
+                set; get;
+            }
+            public Node(int data)
+            {
+                Data = data;
+                Next = null;
+            }
+        }
+
+        Node head = null;
+        int count = 0; // 0 pos 1 elem
+
+        public Node Find(int posit)
+        {
+            if (posit >= count) return null;
+
+            int i = 0;
+            Node P = head;
+
+            while (P != null && i < posit)
+            {
+                P = P.Next;
+                i++;
+            }
+
+            if (i == count) return P;
+            else return null;
+        }
+        public void Addit(int value)
+        {
+            if (head == null)
+            {
+                head = new Node(value);
+            }
+
+            else
+            {
+                Node last = Find(count - 1);
+                last.Next = new Node(value);
+
+            }
+            count++;
+        }
+        public void Insert(int posit, int value)
+        {
+            if (posit < 0 || posit > count)
+            {
+                throw new ArgumentOutOfRangeException($"Invalid position {posit} for insertion");
+            }
+
+            if (posit <= count && head != null)
+            {
+                Node prev = Find(posit - 1);
+                Node old_next = prev.Next;
+                Node new_next = new Node(value);
+                prev.Next = new_next;
+                new_next.Next = old_next;
+                count++;
+            }
+            else if (head == null)
+            {
+                head = new Node(value);
+                count++;
+            }
+
+        }
+        public void Delete(int posit)
+        {
+            if (posit < 0 || posit >= count)
+            {
+                throw new ArgumentOutOfRangeException($"Invalid position {posit} for deletion");
+            }
+
+            if (posit < count && posit > 0)
+            {
+                Node prev = Find(posit - 1);
+                Node GoodBye = Find(posit);
+                prev.Next = GoodBye.Next;
+                count--;
+            }
+            else if (posit == 0 && head != null)
+            {
+                head = null;
+                count--;
+            }
+        }
+        public void Clr()
+        {
+            while (count != 0)
+            {
+                Node vanished = Find(count - 1);
+                vanished.Next = null;
+                count--;
+            }
+            head = null;
+        }
+        public int Count
+        {
+            get { return count; }
+        }
+
+        public int this[int i]
+        {
+            get
+            {
+                if (count <= i || i < 0)
+                {
+                    throw new IndexOutOfRangeException($"There is no index {i} in list");
+                }
+
+                Node th = Find(i);
+
+                return th.Data;
+            }
+
+            set
+            {
+                if (count <= i || i < 0)
+                {
+                    throw new IndexOutOfRangeException($"There is no index {i} in list");
+                }
+
+                Node th = Find(i);
+                th.Data = value;
+            }
+        }
+        public void Shw()
+        {
+            Node cur = head;
+            while (cur != null)
+            {
+                Console.Write($"{cur.Data} ");
+                cur = cur.Next;
+            }
+        }
+    }
+}
